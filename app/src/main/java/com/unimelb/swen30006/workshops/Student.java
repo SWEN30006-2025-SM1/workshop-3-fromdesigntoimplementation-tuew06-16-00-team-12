eb8a85310ebbf17f115218f7819e0ed6638f2d00
Tammy <mytam.le1@student.unimelb.edu.au>
2025-03-25 17:43:08 +1100

    Create Person and Staff classes

---
+++ app/src/main/java/com/unimelb/swen30006/workshops/Student.java
@@ -0,0 +1,10 @@
+package com.unimelb.swen30006.workshops;
+
+public class Student extends Person {
+    private String email;
+    private String studentID;
+
+    public void sendEmailMsg(String) {
+
+    }
+}
